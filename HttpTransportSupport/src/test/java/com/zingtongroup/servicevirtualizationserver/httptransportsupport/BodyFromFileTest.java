7e8307fae24058c771292db8f6064580c83a81eb
kejsa <Fingal95>
2020-11-10 19:02:13 +0100

    Added test as a reminder to test this thoroughly

---
+++ HttpTransportSupport/src/test/java/com/zingtongroup/servicevirtualizationserver/httptransportsupport/BodyFromFileTest.java
@@ -0,0 +1,11 @@
+package com.zingtongroup.servicevirtualizationserver.httptransportsupport;
+
+import org.junit.Test;
+
+public class BodyFromFileTest extends ServerTestBase {
+
+    @Test
+    public void checkFileBody() throws Exception {
+        throw new Exception("Not yet implemented");
+    }
+}
